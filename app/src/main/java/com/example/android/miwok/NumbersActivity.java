367cbd5514a9af13079af979c7b1ef279ac06b7c
wuhyn <hong.yang.ng@gmail.com>
2018-03-19 15:56:29 +1100

    Display numbers through view

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -28,16 +32,24 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumbersActivity", "Words at index 0: " + words.get(0));
-        Log.v("NumbersActivity", "Words at index 1: " + words.get(1));
-        Log.v("NumbersActivity", "Words at index 2: " + words.get(2));
-        Log.v("NumbersActivity", "Words at index 3: " + words.get(3));
-        Log.v("NumbersActivity", "Words at index 4: " + words.get(4));
-        Log.v("NumbersActivity", "Words at index 5: " + words.get(5));
-        Log.v("NumbersActivity", "Words at index 6: " + words.get(6));
-        Log.v("NumbersActivity", "Words at index 7: " + words.get(7));
-        Log.v("NumbersActivity", "Words at index 8: " + words.get(8));
-        Log.v("NumbersActivity", "Words at index 9: " + words.get(9));
+        //Point to the rootView in numbersActivity.xml
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        //Setting up while loop
+        int index = 0;
+
+        while(index < words.size()){
+            TextView wordView = new TextView(this);
+            //Get different index
+            wordView.setText(words.get(index));
+
+            //Add view
+            rootView.addView(wordView);
+
+            //Update index
+            index++;
+        }
+
 
     }
 }
