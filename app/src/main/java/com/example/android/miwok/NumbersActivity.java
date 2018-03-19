1e366834530455e5a50a3f84cd7363a98911264d
wuhyn <hong.yang.ng@gmail.com>
2018-03-19 13:17:55 +1100

    Add ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create arraylist called words
+        ArrayList<String> words = new ArrayList<>();
+
+        //Add words to arraylist
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumbersActivity", "Words at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Words at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Words at index 2: " + words.get(2));
+        Log.v("NumbersActivity", "Words at index 3: " + words.get(3));
+        Log.v("NumbersActivity", "Words at index 4: " + words.get(4));
+        Log.v("NumbersActivity", "Words at index 5: " + words.get(5));
+        Log.v("NumbersActivity", "Words at index 6: " + words.get(6));
+        Log.v("NumbersActivity", "Words at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Words at index 8: " + words.get(8));
+        Log.v("NumbersActivity", "Words at index 9: " + words.get(9));
+
     }
 }
